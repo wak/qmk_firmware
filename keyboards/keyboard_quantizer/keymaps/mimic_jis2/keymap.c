195f71452f27c6fb26c0e3bbce7c18c541ee308a
WAKABAYASHI Hiroaki <wakabayashi.hiroaki@gmail.com>
2021-04-04 17:02:21 +0900

    change keymap

--- keyboards/keyboard_quantizer/keymaps/mimic_jis2/keymap.c
+++ keyboards/keyboard_quantizer/keymaps/mimic_jis2/keymap.c
@@ -16,6 +16,7 @@
 #include QMK_KEYBOARD_H
 #include "keymap_jp.h"
 #define MIMIC_JIS_KEEP_SCREEN 1
+#define MIMIC_JIS_ENABLE_CONTROL_ZKHK 1
 #include "../../../../mimic_jis2/keymap.c"
 
 // Defines names for use in layer keycodes and the keymap
@@ -35,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4,     KC_F5, KC_F6, KC_F7, KC_F8,     KC_F9, KC_F10, KC_F11, KC_F12,    KC_PSCR, KC_SCROLLLOCK, KC_PAUS,
         MY_TILD_BQUOT, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, MY_MIN_USCRE, MY_EQL_PLUS, JP_YEN, KC_BSPACE, KC_INSERT, KC_HOME, KC_PGUP,       KC_NUMLOCK, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS,
         KC_TAB,   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, JP_LBRC, JP_RBRC,           KC_ENT,   KC_DEL, KC_END,  KC_PGDN,          KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_PLUS,
-        KC_CAPS,   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, MY_QUOT_DQUOT, JP_RBRC,                                                  KC_KP_4, KC_KP_5, KC_KP_6,
+        KC_ZKHK,   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, MY_QUOT_DQUOT, JP_RBRC,                                                  KC_KP_4, KC_KP_5, KC_KP_6,
         MY_SHIFT,    KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, JP_SLSH, JP_BSLS, MY_SHIFT,                 KC_UP,                     KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_ENTER,
         MY_CONTROL, KC_LGUI, KC_LALT, JP_MHEN, LOSP,  RAEN, JP_KANA, KC_RALT, KC_RGUI, KC_APP, MY_CONTROL,            KC_LEFT,  KC_DOWN, KC_RIGHT,       KC_KP_0,          KC_KP_DOT
     )
